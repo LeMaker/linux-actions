6f938cd06caa72e3577dde713c49363ab622b495
peter.chen <peter.chen@lemaker.com>
2016-02-17 18:06:28 -0800

    usb: uvc: recover the mmap buf usage for android vender uvc and adjust the uvc err log
    Signed-off-by: peter.chen <peter.chen@lemaker.com>

--- drivers/media/usb/uvc/uvc_video.c
+++ drivers/media/usb/uvc/uvc_video.c
@@ -1000,7 +1000,6 @@ static int uvc_video_decode_start(struct uvc_streaming *stream,
 			  "set).\n");
 		/*added for one frame split to two parts, or one frame with err data problems, ActionsCode(author:liyuan, change_code)*/	  
 		buf->error = UVC_BUF_ERR_ISOFRAM_ERR;
-		uvc_trace(UVC_TRACE_FRAME_ERR, "## USB isochronous frame err .\n");
 	}
 
 	/* Synchronize to the input stream by waiting for the FID bit to be
@@ -1060,7 +1059,6 @@ static int uvc_video_decode_start(struct uvc_streaming *stream,
 		if((buf->bytesused != buf->length) &&
 		   !(stream->cur_format->flags & UVC_FMT_FLAG_COMPRESSED)){
 			buf->error = UVC_BUF_ERR_NOTFULL;
-			uvc_trace(UVC_TRACE_FRAME_ERR, "## Frame complete (not full).\n");
 		}
 		return -EAGAIN;
 	}
@@ -1093,7 +1091,6 @@ static void uvc_video_decode_data(struct uvc_streaming *stream,
 
 	/* Complete the current frame if the buffer size was exceeded. */
 	if (len > maxlen) {
-		uvc_trace(UVC_TRACE_FRAME_ERR, "## Frame complete (overflow).\n");
 		buf->state = UVC_BUF_STATE_READY;
 		/*added for one frame split to two parts, or one frame with err data problems, ActionsCode(author:liyuan, change_code)*/
 		buf->error = UVC_BUF_ERR_OVERFLOW;
@@ -1174,7 +1171,6 @@ static void uvc_video_decode_isoc(struct urb *urb, struct uvc_streaming *stream,
 			if (buf != NULL){
 			        /*added for one frame split to two parts, or one frame with err data problems, ActionsCode(author:liyuan, change_code)*/
 				buf->error = UVC_BUF_ERR_ISOFRAM_LOST;
-				uvc_trace(UVC_TRACE_FRAME_ERR, "## USB isochronous frame lost .\n");
 			}
 			continue;
 		}
@@ -1206,7 +1202,6 @@ static void uvc_video_decode_isoc(struct urb *urb, struct uvc_streaming *stream,
 			      UVC_FMT_FLAG_COMPRESSED)){
 				/*added for one frame split to two parts, or one frame with err data problems, ActionsCode(author:liyuan, change_code)*/  
 				buf->error = UVC_BUF_ERR_NOTFULL;
-				uvc_trace(UVC_TRACE_FRAME_ERR, "## Frame complete (not full).\n");
 			}
 			buf = uvc_queue_next_buffer(&stream->queue, buf);
 		}
